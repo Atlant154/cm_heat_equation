04a40826a70ab4391f699184e65501a59205e5d0
Пацакула Никита <nicman2222@gmail.com>
2018-09-08 16:07:13 +0700

    Basic structure implemented.

---
+++ src/cm_heat_equation_app.erl
@@ -0,0 +1,11 @@
+-module(cm_heat_equation_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	cm_heat_equation_sup:start_link().
+
+stop(_State) ->
+	ok.
