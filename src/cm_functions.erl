30e1034b5fb021aeda1205924d6b2379aa13859c
Пацакула Никита <nicman2222@gmail.com>
2018-09-09 14:01:24 +0700

    Specs defined. Function of heat equation added.

--- src/cm_functions.erl
+++ src/cm_functions.erl
@@ -1,12 +1,15 @@
 -module(cm_functions).
 -export([tridiagonal_matrix_algorythm/4]).
 -export([get_calculation_list/1]).
+-export([get_free_part/3]).
+-export([function_of_heat_sources/2]).
 -export([test/1]).
 
--record(calculation, {
-                        time :: float(),
-                        list_of_results :: list()
-}).
+-define(THERMAL_DIFFUSIVITY, 0.0021).
+-define(TIME_LEFT_BOUND, 0).
+-define(TIME_RIGHT_BOUND, 1).
+-define(X_LEFT_BOUND, 0).
+-define(X_RIGHT_BOUND, 1).
 
 %% Tridiagonal matrix algorythm.
 
@@ -46,6 +49,30 @@ get_tdma_result([P | PTail], [Q | QTail], [X | _XTail] = Results) ->
 
 %% Heat equation.
 
+-spec exact_solution(float(), float()) -> float().
+exact_solution(XCoord, Time) ->
+    (-2) * math:pow(XCoord, 4) - 3 * math:pow(Time, 3) + 3 * math:pow(Time, 2) * XCoord + math:exp(XCoord).
+
+-spec function_of_heat_sources(float(), float()) -> float().
+function_of_heat_sources(XCoord, Time) ->
+    math:pow(XCoord, 2) * (math:pow(XCoord, 2) - 12 * ?THERMAL_DIFFUSIVITY * Time) + math:exp(XCoord) *
+    (XCoord * Time * (?THERMAL_DIFFUSIVITY * Time - 2) + ?THERMAL_DIFFUSIVITY * math:pow(Time, 2) +
+    2 * Time).
+
+-spec get_free_part(float(), float(), list()) -> list().
+get_free_part(?TIME_LEFT_BOUND, H, _PreviousTimeLayerFree) ->
+    get_init_free_part(H);
+get_free_part(_Else, _H, PreviousTimeLayerFree) ->
+    PreviousTimeLayerFree.
+
+get_init_free_part(H) ->
+    get_init_free_part(?X_LEFT_BOUND, ?X_RIGHT_BOUND, H, []).
+
+get_init_free_part(Iter, End, _H, Result) when Iter > End->
+    Result;
+get_init_free_part(Iter, End, H, Result) ->
+    get_init_free_part(Iter + H, End, H, [exact_solution(Iter, 0) | Result]).
+
 -spec get_calculation_list(float()) -> list().
 get_calculation_list(Tau) ->
     get_calculation_list(0.0, 1, [], Tau).
@@ -63,10 +90,10 @@ test(NumOfMeasurement) ->
 tridiagonal_matrix_algorythm_test(Iter, Iter, Val) ->
     Val / Iter;
 tridiagonal_matrix_algorythm_test(TIter, Iter, Val) ->
-    Below = [-3, -5, -6, -5],
-    Abowe = [-1, -1, 2, -4],
-    Main = [2, 8, 12, 18, 10],
-    Free = [-25, 72, -69, -156, 20],
-    {Time, _CalcRes} = timer:tc(cm_functions, tridiagonal_matrix_algorythm, [Below, Main, Abowe, Free]),
-    % io:format("Calc result = ~p.~n", [CalcRes]),
+    Below = [rand:uniform(100) || _ <- lists:seq(1, 1000)],
+    Abowe = [rand:uniform(100) || _ <- lists:seq(1, 1000)],
+    Main = [rand:uniform(100) || _ <- lists:seq(1, 1001)],
+    Free = [rand:uniform(100) || _ <- lists:seq(1, 1001)],
+    {Time, CalcRes} = timer:tc(cm_functions, tridiagonal_matrix_algorythm, [Below, Main, Abowe, Free]),
+    io:format("Calc result = ~p.~n", [CalcRes]),
     tridiagonal_matrix_algorythm_test(TIter + 1, Iter, Val + Time).
